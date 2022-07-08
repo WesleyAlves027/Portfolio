be8b66f637d795e5559799443460e6106e2f1d01
Wesley Alves <wesley_alves.santana@hotmail.com>
2022-07-08 15:29:01 -0300

    adicionando responsividade

--- js/app.js
+++
@@ -1,9 +0,0 @@
-import { valida } from './validacao-form'
-
-const inputs = document.querySelectorAll('.formcontato__input')
-
-inputs.forEach(input =>{
-    input.addEventListener('blur', (evento) => {
-        valida(evento.target)
-    })
-})
